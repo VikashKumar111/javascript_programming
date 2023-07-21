9ee00a13c0faa7cdf91369dcd5ed8e0a5523eaf3
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-21 22:49:16 +0530

    add

--- program.js
+++ program.js
@@ -1,9 +1,32 @@
 
-// const num1 = parseInt(prompt('Enter the first number '));
-// const num2 = parseInt(prompt('Enter the second number '));
+
+// const number = prompt('Enter the number: ');
+
+// const result = Math.sqrt(number);
+// console.log(`The square root of ${number} is ${result}`);
+
+
+
+// const number1 = 2.25;
+// const number2 = -4;
+// const number3 = 'hello';
+
+// const result1 = Math.sqrt(number1);
+// const result2 = Math.sqrt(number2);
+// const result3 = Math.sqrt(number3);
+
+// console.log(`The square root of ${number1} is ${result1}`);
+// console.log(`The square root of ${number2} is ${result2}`);
+// console.log(`The square root of ${number3} is ${result3}`);
+
+
+
+
+
+// const num1 = parseInt(prompt('enter the first number'));
+// const num2 = parseInt(prompt('enter the second number'));
 
 // const sum = num1 + num2;
-
 
 // console.log(`The sum of ${num1} and ${num2} is ${sum}`);
 
@@ -13,24 +36,22 @@
 
 
 
-// const number = prompt('Enter the number: ');
+// const number = prompt('Enter the number');
 
-// const result = Math.sqrt(number);
-// console.log(`The square root of ${number} is ${result}`);
+// const squareRoot = Math.sqrt(number);
+
+// console.log(`The Square Root Of ${number} is ${squareRoot}`);
 
 
 
-const number1 = 2.25;
-const number2 = -4;
-const number3 = 'hello';
 
-const result1 = Math.sqrt(number1);
-const result2 = Math.sqrt(number2);
-const result3 = Math.sqrt(number3);
 
-console.log(`The square root of ${number1} is ${result1}`);
-console.log(`The square root of ${number2} is ${result2}`);
-console.log(`The square root of ${number3} is ${result3}`);
 
+const baseValue = prompt('enter the base value of triangle');
+const heightValue = prompt('enter the height value of triangle');
+
+const area = (baseValue * heightValue) / 2;
+
+console.log(`Area of triangle is ${area}`);
 
 
