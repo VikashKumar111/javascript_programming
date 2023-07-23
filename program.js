c4d109f52a59b96a61490140c3e14c9a51edd392
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-23 23:52:37 +0530

    add

--- program.js
+++ program.js
@@ -189,24 +189,36 @@
 
 
 
-let root1, root2;
+// let root1, root2;
 
-let a = prompt("Enter the first number");
-let b = prompt("Enter the second number");
-let c = prompt("Enter the third number");
+// let a = prompt("Enter the first number");
+// let b = prompt("Enter the second number");
+// let c = prompt("Enter the third number");
 
-let discriminant = (b * b) - 4 * a * c; 
+// let discriminant = (b * b) - 4 * a * c; 
 
-if (discriminant > 0) {
-    root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+// if (discriminant > 0) {
+//     root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+//     root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+//     console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+// }
+// else if (discriminant == 0) {
+//     root1 = root2 = -b / (2 * a);
+//     console.log(`The rrots of the quadratic equation are ${root1} and ${root2}`);
+//  }
+
+
+
+
+const number = parseInt(prompt("Enter the number"));
+
+if (number >0) {
+    console.log("Number is positive");
 }
-else if (discriminant == 0) {
-    root1 = root2 = -b / (2 * a);
-    console.log(`The rrots of the quadratic equation are ${root1} and ${root2}`);
- }
-
-
-
+else if (number == 0) {
+    console.log("Number is zero");
+}
+else  {
+    console.log("Number is negative")
+}
 
