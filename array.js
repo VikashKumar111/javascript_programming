33f5c00ca95355731f5c35cc15d76144bd8cbc64
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-24 23:40:12 +0530

    add

--- array.js
+++ array.js
@@ -1,15 +1,29 @@
 
-function removeItemFromArray(array, n){
-    let newArray = [];
-    for (let i = 0; i < array.length; i++){
-        if (array[i] !== n) {
-            newArray.push(array[i]);
-        }
+// function removeItemFromArray(array, n){
+//     let newArray = [];
+//     for (let i = 0; i < array.length; i++){
+//         if (array[i] !== n) {
+//             newArray.push(array[i]);
+//         }
+//     }
+//     return newArray;
+// }
+
+
+// const result = removeItemFromArray([1, 2, 3, 4, 5, 6], 4);
+
+// console.log(result);
+
+
+
+
+function removeItemFromArray(array, n) {
+    const index = array.indexOf(n);
+    if (index > -1) {
+        array.splice(index,1);
     }
-    return newArray;
+    return array;
 }
 
-
-const result = removeItemFromArray([1, 2, 3, 4, 5, 6], 4);
-
+const result = removeItemFromArray([1, 2, 3, 4, 5, 6, 7], 3);
 console.log(result);
