1b0db00e582ab7de2dc587bbabfd69010b1c8baf
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-31 23:43:11 +0530

    add

--- array.js
+++ array.js
@@ -269,18 +269,35 @@
 
 
 
-function getUniqueAfterMerge(arr1, arr2) {
-    let arr = arr1.concat(arr2);
-    let uniqueArr = [];
+// function getUniqueAfterMerge(arr1, arr2) {
+//     let arr = arr1.concat(arr2);
+//     let uniqueArr = [];
 
-    for (let i of arr) {
-        if (uniqueArr.indexOf(i) === -1) {
-            uniqueArr.push(i);
-      }
-    }
+//     for (let i of arr) {
+//         if (uniqueArr.indexOf(i) === -1) {
+//             uniqueArr.push(i);
+//       }
+//     }
+//     console.log(uniqueArr);
+// }
+
+// const array1 = [1, 2, 4, 5, 8, 9];
+// const array2 = [1, 3, 7, 8, 9, 5, 6, 2];
+// getUniqueAfterMerge(array1, array2);
+
+
+
+
+function getUniqueAfterMerge(arr1, arr2) {
+    let arr = [...arr1, ...arr2];
+     
+    let uniqueArr = [...new Set(arr)];
+
     console.log(uniqueArr);
+
 }
 
-const array1 = [1, 2, 4, 5, 8, 9];
-const array2 = [1, 3, 7, 8, 9, 5, 6, 2];
+const array1 = [1, 2, 4, 6, 8, 9, 5];
+const array2 = [2, 6, 4, 9, 6, 5, 2, 0];
+
 getUniqueAfterMerge(array1, array2);
