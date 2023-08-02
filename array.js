c693b336cb16dfffdcfedcd7363d60b12cebb5f6
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-02 23:57:24 +0530

    add

--- array.js
+++ array.js
@@ -348,14 +348,19 @@
 
 
 
-function sum(num) {
-    if (num > 0) {
-        return num + sum(num - 1);
-    } else {
-        return num;
-    }
-}
+// function sum(num) {
+//     if (num > 0) {
+//         return num + sum(num - 1);
+//     } else {
+//         return num;
+//     }
+// }
 
-const number = parseInt(prompt('Enter a positive integer'));
-const result = sum(number);
-console.log(result);
+// const number = parseInt(prompt('Enter a positive integer'));
+// const result = sum(number);
+// console.log(result);
+
+
+
+const a = Math.random();
+console.log(a);
