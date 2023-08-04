298103e6f1240a831b387e2c9f7fd33aff0d506c
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-04 23:49:07 +0530

    add

--- array.js
+++ array.js
@@ -368,5 +368,8 @@
 
 
 
-const a = Math.random() * (10 - 1) + 1;
-console.log(`Random value between 1 and 10 is ${a}`);
+// const a = Math.random() * (10 - 1) + 1;
+// console.log(`Random value between 1 and 10 is ${a}`);
+
+
+
--- program.js
+++ program.js
@@ -236,3 +236,24 @@
 // else {
 //     console.log("Number is negative");
 // }
+
+
+
+
+function factorial(x) {
+    if (x == 0) {
+        return 1;
+    } else {
+        return x * factorial(x - 1);
+    }
+}
+
+const num = prompt('Enter positive number');
+if (num >= 0) {
+    const result = factorial(num);
+    console.log(`The factorial of${num}is ${result}`);
+} else {
+    console.log('Enter positive number');
+}
+
+
