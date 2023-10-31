a74a19267ab831c629dd4b15eb4172b199616953
Vikash Kumar <vikasthakur9310@gmail.com>
2023-10-31 23:56:36 +0530

    add

--- string.js
+++ string.js
@@ -14,8 +14,28 @@
 // console.log(value);
 
 
-function checkPalindrome(string) {
-    const arrayValues = string.split('');
-    const reverseArrayValues = arrayValues.reverse();
-    const reverseString = reverseArrayValues.join('');
+// function checkPalindrome(string) {
+//     const arrayValues = string.split('');
+//     const reverseArrayValues = arrayValues.reverse();
+//     const reverseString = reverseArrayValues.join('');
+//     if (string == reverseString) {
+//         console.log('It is Palindrome')
+//     }
+//     else {
+//         console.log('It is not Palindrome')
+//     }
+// }
+
+// const string = prompt('Entera string:');
+// const value = checkPalindrome(string);
+// console.log(value);
+
+
+
+const string = prompt('Enter a sentence: ');
+const words = string.split(' ');
+words.sort();
+console.log('The sorted words are:');
+for (const element of words) {
+    console.log(element);
 }
