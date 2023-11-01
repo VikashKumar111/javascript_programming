507733f797a48eb8592667bb375e78791dcdf8e7
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-01 23:59:21 +0530

    add

--- string.js
+++ string.js
@@ -73,7 +73,7 @@
 function countString(str, letter) {
     let count = 0;
     for (let i = 0; i < str.length; i++){
-        if (str.chatAt(i) == letter) {
+        if (str.charAt(i) == letter) {
             count += 1;
         }
     }
