d6a8c2bfe901a6d1bb043215edf7de983dfda131
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-02 22:18:45 +0530

    add

--- string.js
+++ string.js
@@ -70,16 +70,30 @@
 // console.log(result);
 
 
+// function countString(str, letter) {
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++){
+//         if (str.charAt(i) == letter) {
+//             count += 1;
+//         }
+//     }
+//     return count;
+// }
+
+
+// const string = prompt('Enter a string: ');
+// const letterToCheck = prompt('Enter a letter to check: ');
+// const result = countString(string, letterToCheck);
+// console.log(result);
+
+
+
+
 function countString(str, letter) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++){
-        if (str.charAt(i) == letter) {
-            count += 1;
-        }
-    }
+    const re = new RegExp(letter, 'g');
+    const count = str.match(re).length;
     return count;
 }
-
 
 const string = prompt('Enter a string: ');
 const letterToCheck = prompt('Enter a letter to check: ');
