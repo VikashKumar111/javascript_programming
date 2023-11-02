bbd217e3c79d038aac5c143c9c81d6b0179a706c
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-02 23:46:54 +0530

    add

--- string.js
+++ string.js
@@ -151,18 +151,35 @@
 // console.log(result);
 
 
+// function checkString(str) {
+//     if (str.startsWith('S') && str.endsWith('G')) {
+//         console.log('The string starts with S and ends with G');
+//     } else if (str.startsWith('S')) {
+//         console.log('The string starts with S but does not end with G');
+//     } else if (str.endsWith('G')) {
+//         console.log('The string ends with G but does not start with S');
+//     } else {
+//         console.log('The string doest not start with S and does not end with G');
+//     }
+// }
+
+// const string = prompt('Enter the string');
+// const result = checkString(string);
+// console.log(result);
+
+
 function checkString(str) {
-    if (str.startsWith('S') && str.endsWith('G')) {
-        console.log('The string starts with S and ends with G');
-    } else if (str.startsWith('S')) {
+    if (/^S/i.test(str) && /G$/i.test(str)) {
+        console.log('the string starts with S and ends with G');
+    } else if (/^S/i.test(str)) {
         console.log('The string starts with S but does not end with G');
-    } else if (str.endsWith('G')) {
-        console.log('The string ends with G but does not start with S');
+    } else if (/G$/i.test(str)) {
+        console.log('the string ends with G but does not start with S');
     } else {
-        console.log('The string doest not start with S and does not end with G');
+        console.log('the string neither starts with S nor ends with G');
     }
 }
 
-const string = prompt('Enter the string');
+const string = prompt('enter the string');
 const result = checkString(string);
 console.log(result);
