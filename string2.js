9117aeef14753ac5b742275bcd7ea7188d5dd601
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-04 23:48:44 +0530

    add

--- string2.js
+++ string2.js
@@ -1,9 +1,14 @@
 
 
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-});
+// const formatter = new Intl.NumberFormat('en-US', {
+//     style: 'currency',
+//     currency: 'USD'
+// });
 
-const result = formatter.format(2500);
+// const result = formatter.format(2500);
+// console.log(result);
+
+
+const number = 1234.5678
+const result = '$ ' + number.toFixed(2);
 console.log(result);
