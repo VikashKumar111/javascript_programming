cbbd9b4e5a55580d9fcb3998e45810fb59f94f98
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-11 23:05:36 +0530

    add

--- array1.js
+++ array1.js
@@ -1,18 +1,18 @@
 
-function removeItemFromArray(array, n){
-    let newArray = [];
-    for (let i = 0; i < array.length; i++){
-        if (array[i] !== n) {
-            newArray.push(array[i]);
-        }
-    }
-    return newArray;
-}
+// function removeItemFromArray(array, n){
+//     let newArray = [];
+//     for (let i = 0; i < array.length; i++){
+//         if (array[i] !== n) {
+//             newArray.push(array[i]);
+//         }
+//     }
+//     return newArray;
+// }
 
 
-const result = removeItemFromArray([1, 2, 3, 4, 5, 6], 4);
+// const result = removeItemFromArray([1, 2, 3, 4, 5, 6], 4);
 
-console.log(result);
+// console.log(result);
 
 
 
