b8638fd2fa4a02a377af215462a87782c59a7b19
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-12 21:56:57 +0530

    add

--- array1.js
+++ array1.js
@@ -17,16 +17,16 @@
 
 
 
-// function removeItemFromArray(array, n) {
-//     const index = array.indexOf(n);
-//     if (index > -1) {
-//         array.splice(index,1);
-//     }
-//     return array;
-// }
+function removeItemFromArray(array, n) {
+    const index = array.indexOf(n);
+    if (index > -1) {
+        array.splice(index,1);
+    }
+    return array;
+}
 
-// const result = removeItemFromArray([1, 2, 3, 4, 5, 6, 7], 3);
-// console.log(result);
+const result = removeItemFromArray([1, 2, 3, 4, 5, 6, 7], 3);
+console.log(result);
 
 
 //
